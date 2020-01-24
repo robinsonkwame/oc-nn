85385736b84a54e7480ca7022b2e5d4d40e4eab9
Kwame Porter Robinson <kporterrobinson@gmail.com>
2020-01-24 10:32:44 -0500

    (WIP) updatees setup.py for better github pip install

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='src',
-    packages=find_packages(),
-    version='0.0.1',
-    description='One class neural networks for Anomaly detection',
-    author='raghav chalapathy',
-    license='MIT',
+    name='ocnn',
+    version='0.0.2',
+    description:"One class neural networks for Anomaly detection",
+    long_description_content_type='text/markdown',
+    author="Raghavendra Chalapathy, Kwame Porter Robinson",
+    author_email='kwamepr@umich.edu',
+    url='https://github.com/robinsonkwame/oc-nn',
+    packages=find_packages(include=['src*']),
+    include_package_data=True,
+    license="BSD license"
 )
