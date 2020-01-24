3cf5b5e9325c019db663e862d2520f08c76ca22b
Kwame Porter Robinson <kporterrobinson@gmail.com>
2020-01-24 10:34:06 -0500

    (WIP) fix bug in setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='ocnn',
     version='0.0.2',
-    description:"One class neural networks for Anomaly detection",
+    description="One class neural networks for Anomaly detection",
     long_description_content_type='text/markdown',
     author="Raghavendra Chalapathy, Kwame Porter Robinson",
     author_email='kwamepr@umich.edu',
